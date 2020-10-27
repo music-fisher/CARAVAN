49f672205893b592688a8c8257bbbf212e0a7978
EC2 Default User <ec2-user@ip-172-31-21-33.ec2.internal>
2020-10-27 05:32:00 +0000

    [Finish]3-6編集機能作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,6 +12,8 @@ class BlogsController < ApplicationController
   end
   def create
     blog = Blog.new(blog_params)
+    # blog_params = {title: "aaaa",category:"bbb",body: "cccc"}
+    # blog = {id: 1, title: "aaaa",category:"bbb",body: "cccc"}
     blog.save
     # 以下を修正
     redirect_to blog_path(blog.id)
@@ -22,7 +24,14 @@ class BlogsController < ApplicationController
 
  def update
    blog = Blog.find(params[:id])
+  # {id: 1, title: "aaaa",category:"bbb",body: "cccc"}
+   
+   
    blog.update(blog_params)
+  # {title: "bbb",category:"bssssssbb",body: "ccdddddcc"}
+   
+   
+  # blog == {id: 1, title: "bbb",category:"bssssssbb",body: "ccdddddcc"}
    redirect_to blog_path(blog)
  end
 
@@ -30,6 +39,10 @@ class BlogsController < ApplicationController
 private
   def blog_params
     params.require(:blog).permit(:title,:category,:body)
+    # create時
+    # params == {sample: "aaa", blog: {title: "aaaa",category:"bbb",body: "cccc"}, ....}
+    # update時
+    # params == {sample: "aaa", blog: {title: "bbb",category:"bssssssbb",body: "ccdddddcc"}, ....}
   end
 end
 
